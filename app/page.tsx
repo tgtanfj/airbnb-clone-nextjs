51d483afc397af7c448885936019aa6ebe47a844
trungtan <trungtan14@gmail.com>
2023-10-14 16:07:34 +0700

    fix bug

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic'
+
 import getCurrentUser from "./actions/getCurrentUser";
 import getListings, { IListingParams } from "./actions/getListing";
 import ClientOnly from "./components/ClientOnly";
